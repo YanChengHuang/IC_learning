5e67575e645f102f3d9e28e74a9e91f63650c155
YanChengHuang <n26101185@gs.ncku.edu.tw>
2023-09-01 15:36:06 +0800

    add IC disign contest folder and complete 106th preround question

---
+++ IC design course/homework/question/HW1/MMS_4num.v
@@ -0,0 +1,19 @@
+module MMS_4num(result, select, number0, number1, number2, number3);
+
+input        select;
+input  [7:0] number0;
+input  [7:0] number1;
+input  [7:0] number2;
+input  [7:0] number3;
+output [7:0] result; 
+
+
+wire [7:0] upper_result;
+wire [7:0] lower_result;	
+
+MMS_2num upper(.result(upper_result), .select(select), .number0(number0), .number1(number1));
+MMS_2num lower(.result(lower_result), .select(select), .number0(number2), .number1(number3));
+MMS_2num final(.result(result), .select(select), .number0(upper_result), .number1(lower_result));
+
+endmodule
+
