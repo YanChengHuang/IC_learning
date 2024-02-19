c27e80361a3448c2ae387c73ea62f12ea2aab995
YanChengHuang <n26101185@gs.ncku.edu.tw>
2024-02-19 20:11:24 +0800

    Revise some minor issues

--- SV project/testbench/test.sv
+++ SV project/testbench/test.sv
@@ -1,14 +1,11 @@
-`include "tb_component.sv"
 program test(LCD_if.TEST LCDif);
 	import tb_component::*;
 	Environment env;
 	initial begin
-      env = new(LCDif, 20);
+		// generate 20 trans
+      	env = new(LCDif, 20);
 		env.build();
 		env.run();
 		env.wrap_up();
-
     end
-	
-  
 endprogram
